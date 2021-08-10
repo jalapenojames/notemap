2eb9f3718964ba6eb2cadb40c3ca51dd21e9d98b
James <jaames.chen@gmail.com>
2021-08-10 12:13:47 -0400

    updating

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,13 @@
 Rails.application.routes.draw do
 
-  # get 'welcome/index'
-  root :to => 'welcome#index'  
-
   namespace :api do 
     namespace :v1 do 
       resources :notes
       resources :users
     end
   end
+
+  # root 'welcome#index'  
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
